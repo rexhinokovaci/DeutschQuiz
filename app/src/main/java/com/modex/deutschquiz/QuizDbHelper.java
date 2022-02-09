e5e15a99bf07b5499b8db20731edce0bf8b7dbe2
rexhino <kovacirexhino@gmail.com>
2022-02-09 23:06:42 +0100

    6th Commit

--- app/src/main/java/com/modex/deutschquiz/QuizDbHelper.java
+++ app/src/main/java/com/modex/deutschquiz/QuizDbHelper.java
@@ -12,7 +12,7 @@ import java.util.ArrayList;
 
 public class QuizDbHelper extends SQLiteOpenHelper {
 
-    public static final String DATABASE_NAME = "quizShqip.db";
+    public static final String DATABASE_NAME = "deutschquiz.db";
     public static final int DATABASE_VERSION = 1;
 
 
